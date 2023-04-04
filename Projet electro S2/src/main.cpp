fb3db3c23d7cc5b385d19110d08395831d4d9ccb
Léon <dalleleon@gmail.com>
2023-04-04 21:43:24 +0200

    Test de la fonction PulseIn, marche bien mais manque encore de qqch pour stabiliser le résultat

--- Projet electro S2/src/main.cpp
+++ Projet electro S2/src/main.cpp
@@ -2,16 +2,25 @@
 //get analogic signal from pin AO
 int pinAO = A0;
 int value = 0;
+int pin = 9;
+unsigned long duration;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(3, OUTPUT);
+  pinMode(pin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while (value == 0){
-    analogRead(pinAO);
+  //mesure the duration of the pulse
+  duration = pulseIn(pin, HIGH);
+
+  //make an average of 10 values of the duration of the pulse
+  for (int i = 0; i < 10; i++) {
+    duration += pulseIn(pin, HIGH);
   }
-  Serial.println(value);
+  duration = duration / 10;
+
+  Serial.println(duration);
+  duration = 0;
   delay(100);
 }
