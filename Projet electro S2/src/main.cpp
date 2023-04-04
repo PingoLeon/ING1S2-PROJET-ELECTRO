180dd34d7f4a2bd55332bfe8258e5c076aa22a67
Léon <dalleleon@gmail.com>
2023-04-04 14:09:28 +0200

    Test d'un programme pour recupérer le signal et l'envoyer à travers l'arduino sur le buzzer

--- Projet electro S2/src/main.cpp
+++ Projet electro S2/src/main.cpp
@@ -1,10 +1,17 @@
 #include <Arduino.h>
+//get analogic signal from pin AO
+//send it to buzzer on pin 3
 
+int pinAO = A0;
 void setup() {
   // put your setup code here, to run once:
-  //sur ce, dodo
+  pinMode(3, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int value = analogRead(pinAO);
+  Serial.println(value);
+  delay(100);
 }
