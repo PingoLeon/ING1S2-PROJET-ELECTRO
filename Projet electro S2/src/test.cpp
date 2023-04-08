df0b93b47bf370c275fcd8eab858a2198a778597
Léon <dalleleon@gmail.com>
2023-04-08 23:23:11 +0200

    Rajout d'un code (pas encore intégré) pour exploiter le capteur de distance

---
+++ Projet electro S2/src/test.cpp
@@ -0,0 +1,30 @@
+//exemple de code avec un capteur à ultrason HC-SR04
+
+#include <Arduino.h>
+
+const int trigPin = 11;
+const int echoPin = 10;
+
+long duration;
+int distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(100);
+}
