a28855fefe4d9ace9e4c95750d0edfc8cbb4b26f
Léon <dalleleon@gmail.com>
2023-04-14 11:49:28 +0200

    Test du circuit final avec chaque capteur

--- Projet electro S2/src/main.cpp
+++ Projet electro S2/src/main.cpp
@@ -1,79 +1,64 @@
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 
-//initialisation du VCO et du buzzer
-const int inputPin = 12; // Entrée du signal de fréquence lue
-const int BuzzerPin = 2; // Entrée du signal de fréquence envoyée
+//!##################################################  DECLARATION DES PINS ET VARIABLES GLOBALES ##################################################
+//oled screen
+#define OLED_RESET -1
+Adafruit_SSD1306 display(OLED_RESET);
 
-//exemple de code avec un capteur à ultrason HC-SR04
-const int trigPin = 11;
-const int echoPin = 10;
-long duration;
-int distance;
+//bouton
+const int ButtonPin = 4;
+bool buttonState = false;
+
+//ultrason
+const int TriggerPin = 11;
+const int EchoPin = 12;
+
+//Potentiometre
+const int PotPin1 = A0;
+const int PotPin2 = A1;
+
+//buzzer
+const int BuzzerPin = 2;
+//!############################################################################################################################
 
 
-//!REREGLER LES PLAGES DE FREQUENCE POUR LE BUZZER (pq sinon c'est moche)
-void toneif(float freq){
-  if(freq <= 650){
-    tone(BuzzerPin, 261.63);
-  }else if(freq <= 750 && freq > 650){
-    tone(BuzzerPin, 293.66);
-  }else if(freq <= 850 && freq > 750){
-    tone(BuzzerPin, 329.63);
-  }else if(freq <= 950 && freq > 850){
-    tone(BuzzerPin, 349.23);
-  }else if(freq <= 1050 && freq > 950){
-    tone(BuzzerPin, 392.00);
-  }else if(freq <= 1150 && freq > 1050){
-    tone(BuzzerPin, 440.00);
-  }else if(freq <= 3000 && freq > 1150){
-    tone(BuzzerPin, 493.88);
-  }
-}
-
+//?##################################################  SETUP  ##################################################
 void setup() {
-  Serial.begin(9600); // Initialisation de la communication série
+  //? Initialisation des pins
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  delay(2000);
+  pinMode(ButtonPin, INPUT);
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+  pinMode(BuzzerPin, OUTPUT);
+  pinMode(PotPin1, INPUT);
+  pinMode(PotPin2, INPUT);
 
-  //initialisation du VCO et du buzzer
-  pinMode(BuzzerPin, OUTPUT); // Initialisation de la broche de sortie du signal de fréquence
-  pinMode(inputPin, INPUT); // Initialisation de la broche d'entrée du signal de fréquence
-
-  //initialisation du capteur à ultrason
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  //? Initialisation des paramètres de l'ecran
+  display.clearDisplay();
+  display.setRotation(2);
+  display.setTextSize(0.5);
+  display.setCursor(0,0);
+  display.setTextColor(SSD1306_WHITE);
 }
+//?############################################################################################################
 
+
+//*##################################################  LOOP  ##################################################
 void loop() {
-
-  //moyenne de 100 fréquences pour le VCO
-  float freqSum = 0;
-  int count = 0;
-  for (int i = 0; i < 100; i++) {
-    int pulseWidth = pulseIn(inputPin, HIGH, 1000000); // Mesure de la largeur d'impulsion du signal
-    float frequency = 1000000.0 / pulseWidth; // Calcul de la fréquence en Hz
-    if (frequency != INFINITY) { 
-
-      // Vérification que la fréquence mesurée n'est pas "inf"
-      freqSum += frequency;
-      count++;
-
-    }else{
-
-      //Si la fréquence est infinie, on coupe le buzzer car c'est une valeur inexploitable
-      noTone(BuzzerPin);
-
-    }
-  }
-  if (count > 0) {
-    // Calcul de la fréquence moyenne et affichage sur le moniteur série
-    float averageFreq = freqSum / count;
-    Serial.print("Fréquence moyenne : ");
-    Serial.print(averageFreq);
-    Serial.println(" Hz");
-
-    //send the frequency to the test tone
-    toneif(averageFreq);
-  }
-  delay(8);
+  //!print text and flip the screen with setRotation(2) every 2 seconds
+  display.clearDisplay();
+  display.setRotation(0);
+  display.setCursor(0,0);
+  display.println("Hello World!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  display.setRotation(2);
+  display.setCursor(0,0);
+  display.println("Hello World!");
+  display.display();
+  delay(2000);
 }
-
-
+//*############################################################################################################
